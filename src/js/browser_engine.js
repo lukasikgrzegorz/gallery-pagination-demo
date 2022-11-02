d1eaca002f514b0b66a8ba54dd6497072b34814a
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-02 20:59:27 +0100

    finish inifinty scroll
    add toogle manual/auto

--- src/js/browser_engine.js
+++ src/js/browser_engine.js
@@ -1,8 +1,8 @@
+import _throttle from 'lodash.throttle';
 import axios from "axios";
 import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 
 const APIKEY = "31019872-5203125bb9147bf7b31b034ba"
 const gallery = document.querySelector(".gallery");
@@ -13,7 +13,7 @@ const btnLoop = document.querySelector(".loop-btn");
 let actualPage = 1;
 let currentSearchName = "";
 let lastPage = 1;
-let scrollToogle = false;
+let autoScrollToogle = false;
 
 let lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
@@ -23,24 +23,28 @@ const fetchPhotos = async (name,page) => {
     try {
         const respone = await axios(`https://pixabay.com/api/?key=${APIKEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
         const photos = await respone.data;
-        if (photos.hits.length === 0) { 
+        checkResults(photos);
+    } catch(error) {
+        console.log(error.message);
+        Notiflix.Notify.failure(error.message);
+    }
+};
+
+const checkResults = (photos) => {
+     if (photos.hits.length === 0) { 
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");   
         } else {    
             if (actualPage === 1) {
                 Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
                 currentSearchName = name;
                 lastPage = Math.ceil(photos.totalHits / 40);
-                if (lastPage > 1 && scrollToogle===false) { btnMore.classList.add("is-visible") };
+                if (lastPage > 1 && autoScrollToogle===false){ btnMore.classList.add("is-visible")};
             }
             renderPhotos(photos);
             lightbox.refresh();
-            if (actualPage > 1) {scrollPage()};
+            if (actualPage > 1) {scrollAfterRender()};
         }   
-    } catch(error) {
-        console.log(error.message);
-        Notiflix.Notify.failure(error.message);
-    }
-};
+}
 
 const renderPhotos =(photos) => {
     const markup = photos.hits.map((photo) => {
@@ -60,11 +64,47 @@ const renderPhotos =(photos) => {
     gallery.innerHTML = gallery.innerHTML+markup;
 }
 
+const scrollAfterRender = () => {
+    const photoInfo = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } = photoInfo;
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    })
+}
+ 
+const infinityScroll = _throttle(() => {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        if (actualPage < lastPage) {
+            actualPage++;
+            fetchPhotos(currentSearchName, actualPage);     
+            if (actualPage === lastPage) {
+                Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); 
+                btnMore.classList.remove("is-visible");
+            }  
+        } 
+    }
+},300)
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     actualPage = 1;
     gallery.innerHTML = "";
     fetchPhotos(form.searchQuery.value, actualPage);
+})
+
+btnLoop.addEventListener("click", () => {
+    if (autoScrollToogle === false) {
+        autoScrollToogle = true;
+        window.addEventListener('scroll', infinityScroll);
+        Notiflix.Notify.info("Loading method has been changed to auto.");
+    } else {
+        autoScrollToogle = false;
+        window.removeEventListener('scroll', infinityScroll);
+        Notiflix.Notify.info("Loading method has been changed to manual.");    
+    }  
+    btnLoop.classList.toggle("is-active");
+    if (actualPage!=lastPage){btnMore.classList.toggle("is-visible");} 
 })
 
 btnMore.addEventListener("click", () => {
@@ -76,33 +116,4 @@ btnMore.addEventListener("click", () => {
     }  
 })
 
-const scrollPage = () => {
-    const photoInfo = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-    const { height: cardHeight } = photoInfo;
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    })
-}
-
-    
-const infinityScroll= () => {
-    if(window.scrollY + window.innerHeight >= 
-    document.documentElement.scrollHeight){
- 
-    if (actualPage < lastPage) {
-          actualPage++;
-    fetchPhotos(currentSearchName, actualPage);     
-    if (actualPage === lastPage) {
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); 
-        btnMore.classList.remove("is-visible");
-    }  
-    } 
-    }
-}
-
-btnLoop.addEventListener("click", () => {
-    scrollToogle = true;
-    window.addEventListener('scroll', infinityScroll);
-})
 
